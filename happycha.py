d34fc3ed2f178a3e7c6d8a146fac641be5d68262
Joseph-2 <91758003+Joseph-2@users.noreply.github.com>
2021-10-27 13:54:03 -0700

    Update happycha.py

--- happycha.py
+++ happycha.py
@@ -1,14 +1,19 @@
 #imports
 import turtle
 import random
-#variables
-
 #import config
 background = turtle.Screen()
-gravestone = turtle.Turtle()
 background.setup(width=800,height=600)
 background.bgpic("dribbble.gif")
 background.addshape("gravestone.gif")
+background.addshape("ghost.gif")
+#ghost
+ghost = turtle.Turtle()
+ghost.penup()
+ghost.hideturtle()
+ghost.shape("ghost.gif")
+#gravestone
+gravestone = turtle.Turtle()
 gravestone.shape("gravestone.gif")
 gravestone.penup()
 gravestone.goto(-300,-250)
